1fdba0ca7dd7e96d46e1e78a80ac5a558f747dc3
mayankdama <mayank.dama@ever.ag>
2025-05-30 12:10:48 +0530

    - YML config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-buildscript {
-    dependencies {
-        classpath(libs.google.oss.licensesPlugin)
-    }
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.gms) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.apollo.graphql) apply false
-}
+    plugins {
+    }
