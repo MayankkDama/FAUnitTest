22f8f14a110d787d662771979e432a4dd278e8d9
mayankdama <mayank.dama@ever.ag>
2025-05-30 12:00:13 +0530

    - Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,55 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenLocal()
+        google()
+        mavenCentral()
+        maven {
+            setUrl("https://github.com/Gainsight/px-android/raw/main/")
+        }
+        maven {
+            setUrl("https://jitpack.io")
+            credentials {
+                username = "x-access-token"
+                password = System.getenv("JITPACK_GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+rootProject.name = "FieldAlytics"
+include(":app")
+
+include(":common:ui")
+include(":common:data")
+include(":common:utils")
+include(":common:domain")
+
+include(":main-bottom-bar:ui")
+
+include(":settings:ui")
+
+include(":profiles:ui")
+
+include(":onboarding:ui")
+include(":onboarding:data")
+include(":onboarding:domain")
+
+include(":dashboard:ui")
+include(":dashboard:data")
+include(":dashboard:domain")
+
+// include(":MarketKit-Android")
